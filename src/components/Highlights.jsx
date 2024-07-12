1abf7d3dbbc3ac1f79a41d99c246a8a1044801ee
Kaustubh <kaustubhkumbhare02@gmail.com>
2024-07-12 21:58:49 +0530

    complete-highlight-section

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { rightImg, watchImg } from '../utils'
+import VideoCarousel from './VideoCarousel'
 
 const Highlights = () => {
+
+    useGSAP(() => {
+        gsap.to('#title', {
+            opacity: 1,
+            delay: 1,
+            y: 0
+        })
+        gsap.to('.link', {
+            opacity: 1,
+            y: 0,
+            duration: 1,
+            stagger:0.25
+        })
+
+    }, [])
+
     return (
-        <div>
-           
-        </div>
+        <section id='hightlights' className='w-screen overflow-hidden h-full common-padding bg-zinc'>
+            <div className='screen-max-width'>
+                <div className='mb-12 w-full md:flex items-center justify-between'>
+                    <h1 id='title' className='section-heading'>Get the hightlights.</h1>
+                    <div className="flex flex-wrap items-end gap-5">
+                        <p className="link">
+                            Watch the film
+                            <img src={watchImg} alt="watch" className="ml-2" />
+                        </p>
+                        <p className="link">
+                            Watch the event
+                            <img src={rightImg} alt="right" className="ml-2" />
+                        </p>
+                    </div>
+                </div>
+
+                <VideoCarousel/>
+            </div>
+        </section >
     )
 }
 
---
+++ src/components/VideoCarousel.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const VideoCarousel = () => {
+  return (
+    <div>
+      ssup
+    </div>
+  )
+}
+
+export default VideoCarousel
